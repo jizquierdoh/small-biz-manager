65b9904d97b43f9072494dfd438b494a3e428d7f
Jhonathan Izquierdo Higuita <jizquierdoh@outlook.com>
2022-04-04 21:57:08 -0500

    Hide content when user is not signed in.

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,14 +1,25 @@
+// Hooks
+import { useStoreForApp } from '../../store';
+
 // Components
 import MainForm from '../main-form/MainForm';
 import RegistersTable from '../registers-table/RegistersTable';
 
 const Main = ({}) => {
+	const currentUser = useStoreForApp((store) => store.currentUser);
+
 	return (
 		<>
-			<main className="w-full p-2">
-				<MainForm />
-				<RegistersTable />
-			</main>
+			{currentUser ? (
+				<main className="w-full p-2">
+					<MainForm />
+					<RegistersTable />
+				</main>
+			) : (
+				<main className="w-full p-2">
+					<p>Por favor inicie sesión para usar la App.</p>
+				</main>
+			)}
 		</>
 	);
 };
