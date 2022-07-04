c3413d65bb70fe9f8e42a10c697cd1b3ae7e19b5
Jhonathan Izquierdo Higuita <jizquierdoh@outlook.com>
2022-07-04 17:35:30 -0500

    Added progressbar when building.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import progress from 'vite-plugin-progress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    progress()
+  ]
 })
